074a4064ffe18c4dca8bf2c4a54336e5fab2de58
Azeem Amjad <107496010+azeemamjad@users.noreply.github.com>
2024-11-08 11:27:07 +0500

    LabMids
    Questions: 1. Create a WPF Application that Calculates the age of a person by using its birthday.           
    solution: LabMids/Birthday_Calculator          
     it is a wpf application. 
    
    2. Write a c# program that checks if the given string is a palindrome or not.           
    solution: LabMids/Palindrom_Program          
     it is a console application.

---
+++ Birthday_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Birthday_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void datetimepicker__SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime date = DateTime.Now;
+            DateTime? dateofbirth = datetimepicker_.SelectedDate;
+            TimeSpan? age = date - dateofbirth;
+            if (age.HasValue)
+            {
+                TimeSpan days = age.Value;
+                double number_of_days = Convert.ToInt64(days.TotalDays);
+                agetxtblock.Text = get_age(number_of_days);
+            }
+
+        }
+
+        string get_age(double days)
+        {
+            Age age = new Age();
+            int years = Convert.ToInt32(days / 365);
+            age.years = years; 
+            days = days % 365;
+
+            int mounths = Convert.ToInt32(days / 30);
+            age.mounths = mounths;
+            days = days % 30;
+
+            age.days = Convert.ToInt32(days);
+
+            return age.ToString();
+        }
+    }
+    class Age
+    {
+        public int years {  get; set; } 
+        public int mounths {  get; set; } 
+        public int days {  get; set; } 
+        public Age() { }
+
+        public override string ToString()
+        {
+            return $"Your age is {years} Years, {mounths} Months and {days} Days.";
+        }
+    }
+}
